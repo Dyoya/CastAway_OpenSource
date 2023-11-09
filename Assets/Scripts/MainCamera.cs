56391ed3aea61031900b98b04bdeed57d00285a0
Hyeong Min <118094189+Dyoya@users.noreply.github.com>
2023-11-09 11:42:21 +0900

    HM_test

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    public float followSpeed = 5f;
+
+    public float offsetX = 0f;
+    public float offsetY = 5f;
+    public float offsetZ = -10f;
+
+    Vector3 _cameraPosition;
+
+    private void LateUpdate()
+    {
+        _cameraPosition.x = player.transform.position.x + offsetX;
+        _cameraPosition.y = player.transform.position.y + offsetY;
+        _cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = Vector3.Lerp(transform.position, _cameraPosition, followSpeed * Time.deltaTime);
+    }
+}
