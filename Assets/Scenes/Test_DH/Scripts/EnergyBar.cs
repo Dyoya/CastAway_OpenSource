9424daacda54ba337c1a9de6841b57c35e99a9d4
KDHen <money6845@naver.com>
2023-11-14 20:15:36 +0900

    Add ProgressBar UI

---
+++ Assets/Scenes/Test_DH/Scripts/EnergyBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBar : MonoBehaviour
+{
+    public ProgressBar Pb;
+
+    private void Start()
+    {
+        Pb.BarValue = 100;
+    }
+
+    //스테미너 감소
+    public void DecreaseStamina()
+    {
+        if (Pb.BarValue != 0)
+            Pb.BarValue -= Time.deltaTime * 8;
+    }
+    //스테미너 증가
+    public void RecoverStamina()
+    {
+        if (Pb.BarValue < 100)
+        {
+            Pb.BarValue += Time.deltaTime * 4;
+        }
+    }
+}
---
+++ Assets/Scenes/Test_DH/Scripts/HungryBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungryBar : MonoBehaviour
+{
+    public ProgressBar Pb;
+    //배고픔이 0이 됬는지 안됬는지
+    public bool isHungryZero = false;
+
+    private void Start()
+    {
+        Pb.BarValue = 10;
+    }
+
+    //배고픔 감소(달릴 때)
+    public void isRunningDecreaseHungry()
+    {
+        if (Pb.BarValue != 0)
+            Pb.BarValue -= Time.deltaTime * 1.5f;
+        else if (Pb.BarValue <= 0)
+        {
+            isHungryZero = true;
+        }
+    }
+    //배고픔 감소
+    public void DecreaseHungry()
+    {
+        if (Pb.BarValue < 100)
+        {
+            Pb.BarValue -= Time.deltaTime;
+        }
+    }
+}
