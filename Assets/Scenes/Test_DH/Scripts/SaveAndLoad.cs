738a2e8e667c79fe9356f86ceb4fb24a22fd9e33
KDHen <money6845@naver.com>
2023-11-21 17:18:57 +0900

    Scripts

--- Assets/Scenes/Test_DH/Scripts/SaveAndLoad.cs
+++ Assets/Scenes/Test_DH/Scripts/SaveAndLoad.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 [System.Serializable]
 public class SaveData
 {
     public Vector3 playerPos;
+    public Vector3 playerRot;
+
+    public List<int> inventoryArrayNum = new List<int>();
+    public List<string> inventoryItemName = new List<string>();
+    public List<int> inventoryItemNum = new List<int>();
 }
 
 public class SaveAndLoad : MonoBehaviour
@@ -17,6 +24,8 @@ public class SaveAndLoad : MonoBehaviour
     private string SAVE_FILENAME = "/SaveFile.txt";
 
     private StarterAssets.ThirdPersonController thePlayer;
+    private InventoryUI theInventory;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,8 +39,22 @@ public class SaveAndLoad : MonoBehaviour
     public void SaveData()
     {
         thePlayer = FindObjectOfType<StarterAssets.ThirdPersonController>();
+        theInventory = FindObjectOfType<InventoryUI>();
 
         saveData.playerPos = thePlayer.transform.position;
+        saveData.playerRot = thePlayer.transform.eulerAngles;
+
+
+        slot[] slots = theInventory.getSlots();
+        for(int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null)
+            {
+                saveData.inventoryArrayNum.Add(i);
+                saveData.inventoryItemName.Add(slots[i].item.itemName);
+                saveData.inventoryItemNum.Add(slots[i].itemCount);
+            }
+        }
 
         string json = JsonUtility.ToJson(saveData);
 
@@ -43,15 +66,30 @@ public class SaveAndLoad : MonoBehaviour
 
     public void LoadData()
     {
-        if(File.Exists(SAVE_DATA_DIRECTORY + SAVE_FILENAME))
+        if (File.Exists(SAVE_DATA_DIRECTORY + SAVE_FILENAME))
         {
             string loadJson = File.ReadAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME);
             saveData = JsonUtility.FromJson<SaveData>(loadJson);
-
+            Debug.Log(loadJson);
             thePlayer = FindAnyObjectByType<StarterAssets.ThirdPersonController>();
-            thePlayer.transform.position = saveData.playerPos;
+            theInventory = FindAnyObjectByType<InventoryUI>();
 
-            Debug.Log("로드 완료");
+            if(thePlayer == null)
+            {
+                Debug.Log("thePlayer is null");
+            }
+            else
+            {
+                thePlayer.transform.position = saveData.playerPos;
+                thePlayer.transform.eulerAngles = saveData.playerRot;
+
+                for(int i = 0; i < saveData.inventoryItemName.Count; i++)
+                {
+                    theInventory.LoadToInven(saveData.inventoryArrayNum[i], saveData.inventoryItemName[i], saveData.inventoryItemNum[i]);
+                }
+
+                Debug.Log("로드 완료");
+            }
         }
         else
         {
