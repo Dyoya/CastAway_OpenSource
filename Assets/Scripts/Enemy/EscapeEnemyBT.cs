abdcca8ae17b0c8a6dc5112ba109447634ff2b19
Dyoya <118094189+Dyoya@users.noreply.github.com>
2023-11-23 18:10:45 +0900

    v0.3.4

--- Assets/Scripts/Enemy/EscapeEnemyBT.cs
+++ Assets/Scripts/Enemy/EscapeEnemyBT.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 using UnityEngine.UIElements;
 
 //[RequireComponent(typeof(Animator))]
-public class EnemyBT : MonoBehaviour
+public class EscapeEnemyBT : MonoBehaviour
 {
     Transform _playerTransform;
 
@@ -15,21 +15,14 @@ public class EnemyBT : MonoBehaviour
     [SerializeField] int maxhp = 15;
 
     [Header("Distance")]
-    [SerializeField] float moveSpeed = 6f; // Enemy 이동 속도
-    [SerializeField] float findDistance = 10f; // 플레이어 발견 범위
-    [SerializeField] float attackDistance = 4f; // 플레이어 공격 범위
+    [SerializeField] float moveSpeed = 10f; // Enemy 이동 속도
+    [SerializeField] float findDistance = 5f; // 플레이어 발견 범위
+    [SerializeField] float runDistance = 10f;
 
     [Header("Patrol")]
     [SerializeField] float idlePatrolRange = 10f; // 랜덤 순찰 최대 거리
     [SerializeField] float patrolTime = 10f; // 순찰 지점 변경 시간
-    [SerializeField] float patrolMoveSpeed = 3f;
-
-    [Header("Attack")]
-    [SerializeField] bool isAttackEnemy; //true일 경우 공격형 적대 몬스터
-    //[SerializeField] int attackPower = 3; // Enemy 공격력
-    [SerializeField] float attackCooldown = 2f; // 공격 쿨다운 시간
-    private bool canAttack = true; // 다음 공격이 가능한지 여부를 나타내는 플래그
-    private float lastAttackTime; // 마지막 공격 시간
+    [SerializeField] float patrolMoveSpeed = 6f;
 
 
     [Header("Sound")]
@@ -50,9 +43,6 @@ public class EnemyBT : MonoBehaviour
     const string _IDLE_ANIM_STATE_NAME = "Idle";
     const string _IDLE_ANIM_TRIGGER_NAME = "idle";
 
-    const string _ATTACK_ANIM_STATE_NAME = "Attack";
-    const string _ATTACK_ANIM_TRIGGER_NAME = "attack";
-
     const string _DAMAGED_ANIM_STATE_NAME = "Damaged";
     const string _DAMAGED_ANIM_TRIGGER_NAME = "damaged";
 
@@ -66,7 +56,6 @@ public class EnemyBT : MonoBehaviour
     int _temporaryDamage = 0;
 
     bool isMove = false;
-    bool isReturn = false;
 
     private void Awake()
     {
@@ -120,17 +109,8 @@ public class EnemyBT : MonoBehaviour
                 (
                     new List<INode>()
                     {
-                        new ActionNode(CheckAttacking),
-                        new ActionNode(CheckAttackRange),
-                        new ActionNode(DoAttack),
-                    }
-                ),
-                new SequenceNode
-                (
-                    new List<INode>()
-                    {
                         new ActionNode(CheckFind),
-                        new ActionNode(Chase),
+                        new ActionNode(Run),
                     }
                 ),
                 //new ActionNode(Return),
@@ -159,70 +139,12 @@ public class EnemyBT : MonoBehaviour
         return false;
     }
     #region Public Func
-    public void SetDamage(int  damage)
+    public void SetDamage(int damage)
     {
-        if(_temporaryDamage != 0)
+        if (_temporaryDamage != 0)
         {
             _temporaryDamage = damage;
         }
-    }
-    #endregion
-
-    #region Attack Node
-    INode.ENodeState CheckAttacking()
-    {
-        // 이전 공격 후 일정 시간이 지나면 공격 가능으로 설정
-        if (!canAttack && Time.time - lastAttackTime >= attackCooldown)
-        {
-            canAttack = true;
-        }
-
-        if (IsAnimationRunning(_ATTACK_ANIM_STATE_NAME))
-        {
-            return INode.ENodeState.ENS_Running;
-        }
-
-        return INode.ENodeState.ENS_Success;
-    }
-
-    INode.ENodeState CheckAttackRange()
-    {
-        if (_playerTransform != null) 
-        {
-            // 플레이어가 공격 사거리 안에 들어왔으면
-            if (Vector3.SqrMagnitude(_playerTransform.position - transform.position) < attackDistance * attackDistance)
-            {
-                //Debug.Log("CheckAttackRange : Success");
-                _agent.isStopped = true;
-                _agent.velocity = Vector3.zero;
-                return INode.ENodeState.ENS_Success;
-            }
-        }
-
-        //Debug.Log("CheckAttackRange : Failure");
-        return INode.ENodeState.ENS_Failure;
-    }
-
-    INode.ENodeState DoAttack()
-    {
-        if (_playerTransform != null && canAttack)
-        {
-            // 공격 애니메이션 실행
-            _anim.SetTrigger(_ATTACK_ANIM_TRIGGER_NAME);
-
-            // 플레이어 방향 바라보도록 설정
-            Vector3 temp = (_playerTransform.position - transform.position).normalized;
-            temp.y = 0;
-            transform.forward = temp;
-
-            // 다음 공격이 가능한지 여부 설정
-            canAttack = false;
-            lastAttackTime = Time.time;
-
-            return INode.ENodeState.ENS_Running;
-        }
-
-        return INode.ENodeState.ENS_Failure;
     }
     #endregion
 
@@ -233,7 +155,7 @@ public class EnemyBT : MonoBehaviour
         var overlapColliders = Physics.OverlapSphere(transform.position, findDistance, LayerMask.GetMask("Player"));
 
         // overlapColliders가 1개 이상 -> 플레이어가 감지 됨
-        if (overlapColliders != null && overlapColliders.Length > 0) 
+        if (overlapColliders != null && overlapColliders.Length > 0)
         {
             //isMove = true;
             _playerTransform = overlapColliders[0].transform;
@@ -247,80 +169,45 @@ public class EnemyBT : MonoBehaviour
         //Debug.Log("CheckFind : Failure");
         return INode.ENodeState.ENS_Failure;
     }
-    INode.ENodeState Chase()
+    // TODO : Run으로 변경하기 //
+    INode.ENodeState Run()
     {
         if (_playerTransform != null)
         {
-            // 공격 범위 사거리까지 이동 완료한 경우
-            if (Vector3.SqrMagnitude(_playerTransform.position - transform.position) < attackDistance * attackDistance)
-            {
-                //Debug.Log("Chase : Success");
-                _agent.isStopped = true;
-                _agent.velocity = Vector3.zero;
-                isMove = false; // 이동 상태 해제
-                return INode.ENodeState.ENS_Success;
-            }
-
             if (!isMove) // 이동 중이 아닌 경우에만 Move 애니메이션 호출
             {
                 if (!IsAnimationRunning(_MOVE_ANIM_STATE_NAME))
                     _anim.SetTrigger(_MOVE_ANIM_TRIGGER_NAME);
 
                 isMove = true; // 이동 상태로 설정
-                isReturn = true; // 언제든지 복귀 가능
+                _agent.speed = moveSpeed;
+                _agent.isStopped = false;
+
+                float r = Random.Range(3, runDistance);
+                Vector3 sphere = Random.insideUnitSphere.normalized;
+                Vector3 patrolPosition = gameObject.transform.position + r * sphere;
+                _agent.SetDestination(patrolPosition);
             }
 
-            // 공격 범위 사거리까지 이동 중일 경우
-            //transform.position = Vector3.MoveTowards(transform.position, _playerTransform.position, Time.deltaTime * moveSpeed);
-            _agent.speed = moveSpeed;
-            _agent.isStopped = false;
-            _agent.SetDestination(_playerTransform.position);
+            if (!(_agent.pathPending || _agent.remainingDistance > moveSpeed * 0.3))
+            {
+                isMove = false;
+                _agent.isStopped = true;
+                _agent.velocity = Vector3.zero;
 
-            //Debug.Log("Chase : Running");
+                return INode.ENodeState.ENS_Success;
+            }
+
+            //Debug.Log("Run : Running");
             return INode.ENodeState.ENS_Running;
         }
 
         // 플레이어를 발견 못한 경우
-        //Debug.Log("Chase : Failure");
-        if(isMove)
+        //Debug.Log("Run : Failure");
+        if (isMove)
         {
             isMove = false;
         }
-        return INode.ENodeState.ENS_Failure;
-    }
-    #endregion
-
-    #region Move Origin Pos Node
-    INode.ENodeState Return()
-    {
-        if (isReturn)
-        {
-            // 복귀를 완료 했을 경우
-            if (Vector3.SqrMagnitude(_originPos - transform.position) < 0.5f)
-            {
-                isMove = false;
-                isReturn = false;
-                _agent.isStopped = true;
-                _agent.velocity = Vector3.zero;
-                transform.position = _originPos;
-                transform.rotation = _originRot;
-                //Debug.Log("Return : Success");
-                return INode.ENodeState.ENS_Success;
-            }
-            else
-            {
-                if(!IsAnimationRunning(_MOVE_ANIM_STATE_NAME))
-                    _anim.SetTrigger(_MOVE_ANIM_TRIGGER_NAME);
-
-                //transform.position = Vector3.MoveTowards(transform.position, _originPos, Time.deltaTime * moveSpeed);
-                _agent.isStopped = false;
-                _agent.speed = moveSpeed;
-                _agent.SetDestination(_originPos);
-                //Debug.Log("Return : Running");
-                return INode.ENodeState.ENS_Running;
-            }
-        }
-
         return INode.ENodeState.ENS_Failure;
     }
     #endregion
@@ -358,11 +245,11 @@ public class EnemyBT : MonoBehaviour
             //Debug.Log("Damaged : Failure");
             return INode.ENodeState.ENS_Failure;
         }
-        
+
     }
     void PlayDamagedSound()
     {
-        if(asDamagedSound != null)
+        if (asDamagedSound != null)
         {
             asDamagedSound.Play();
         }
@@ -404,7 +291,7 @@ public class EnemyBT : MonoBehaviour
 
     #region Idle Node
     INode.ENodeState Patrol()
-    {   
+    {
         if (!isMove && _patrolCurrentTime >= patrolTime) // 순찰 시작 전, 처음에만 호출
         {
             _agent.isStopped = false;
@@ -414,7 +301,7 @@ public class EnemyBT : MonoBehaviour
             // 순찰 지점 1개 뽑기
             float r = Random.Range(3, idlePatrolRange);
             Vector3 sphere = Random.insideUnitSphere.normalized;
-            Vector3 patrolPosition =  gameObject.transform.position +  r * sphere;
+            Vector3 patrolPosition = gameObject.transform.position + r * sphere;
             _agent.SetDestination(patrolPosition);
 
             _anim.SetTrigger(_MOVE_ANIM_TRIGGER_NAME);
