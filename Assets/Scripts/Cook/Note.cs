e3cddc8b42270972f803d5c38c4704f96ac3031c
EIVE <myzxcv010242@gmail.com>
2023-11-25 17:16:04 +0900

    v0.3.7

---
+++ Assets/Scripts/Cook/Note.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 100;
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+}
