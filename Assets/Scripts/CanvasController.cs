68241bea5c307f9870c7534a607f8c65765ed2b0
김형준 <gudwns051613@gmail.com>
2023-11-26 22:57:23 +0900

    object done & CraftUI

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public Canvas canvas;
+    private bool isCanvasActive = false;
+
+    void Start()
+    {
+        // 게임 시작 시 Canvas를 꺼놓음
+        canvas.enabled = false;
+    }
+
+    void Update()
+    {
+        // E 키를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            // Canvas가 현재 꺼져있으면 켜고, 켜져있으면 끔
+            isCanvasActive = !isCanvasActive;
+            canvas.enabled = isCanvasActive;
+
+            // 마우스 포인터 보이기/숨기기 설정
+            Cursor.visible = isCanvasActive;
+        }
+    }
+}
