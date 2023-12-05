7ba74849e3ae71580a4dbc151243bbd7aa4ecc04
Hyeong Min <118094189+Dyoya@users.noreply.github.com>
2023-12-05 15:39:09 +0900

    v0.4.8
    - 요리 키 변경 (E -> C)
    - 모닥불 미니게임 추가
    - 모닥불 관련 스크립트 추가

---
+++ Assets/Scripts/Fire/Fire.cs
@@ -0,0 +1,104 @@
+using DG.Tweening;
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField]
+    private int damage;
+
+    [SerializeField] private float damageTime;
+    float _currentDamageTime;
+
+    [SerializeField] private float durationTime;
+    float _currentDurationTime;
+
+    [SerializeField]
+    private ParticleSystem _ps;
+
+    [SerializeField]
+    private GameObject fire;
+
+    bool isFire = false;
+
+    GameObject player;
+    ThirdPersonController _tpc;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        _currentDurationTime = durationTime;
+
+        _tpc = player.GetComponent<ThirdPersonController>();
+    }
+
+
+    void Update()
+    {
+        if(isFire)
+        {
+            ElapseTime();
+        }
+    }
+
+    private void ElapseTime()
+    {
+        _currentDurationTime -= Time.deltaTime;
+
+        if (_currentDurationTime <= 0) 
+        {
+            FireOff();
+        }
+
+        if(_currentDamageTime > 0)
+        {
+            _currentDamageTime -= Time.deltaTime;
+        }
+    }
+    public void addDurationTime(float time)
+    {
+        _currentDurationTime += time;
+    }
+
+    private void FireOff()
+    {
+        _ps.Stop();
+        isFire = false;
+
+        StartCoroutine(SetFire(false));
+    }
+
+    public void FireOn(float time)
+    {
+        _ps.DORestart();
+        isFire = true;
+        _currentDurationTime += time;
+
+        StartCoroutine(SetFire(true));
+    }
+
+    IEnumerator SetFire(bool onoff)
+    {
+        yield return new WaitForSeconds(3f);
+
+        fire.SetActive(onoff);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(isFire && other.transform.tag == "Player")
+        {
+            if(_currentDamageTime <= 0)
+            {
+                Debug.Log("불 데미지");
+                // 플레이어 데미지 주기 코드 작성
+                _tpc.AttackDamage(damage);
+
+                _currentDamageTime = damageTime;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Fire/FireGame.cs
@@ -0,0 +1,86 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using Unity.VisualScripting;
+
+public class FireGame : MonoBehaviour
+{
+    GameObject _player;
+    GameObject FireGameUI;
+    
+    [SerializeField] float goalTime = 10f;
+    [SerializeField] GameObject FireTrigger;
+    [SerializeField] Slider slider;
+    [SerializeField] TextMeshProUGUI TimeText;
+
+    float currentTime;
+    bool isGame;
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player");
+        currentTime = goalTime;
+    }
+
+    private void Update()
+    {
+        if(isGame)
+        {
+            currentTime -= Time.deltaTime;
+            slider.value -= 0.3f * Time.deltaTime;
+
+            // 게임 성공
+            if (slider.value >= 0.99) 
+            {
+                StopGame();
+                FireTrigger.GetComponent<Fire>().FireOn(60f);
+            }
+
+            // 게임 실패
+            if(currentTime <= 0)
+            {
+                currentTime = 0;
+                StopGame();
+            }
+
+            SetText(currentTime);
+        }
+        else
+        {
+            initSetting();
+        }
+    }
+    void SetText(float time)
+    {
+        
+        TimeText.text = "Time : " + Mathf.FloorToInt(time).ToString() + "s";
+    }
+   
+    void initSetting()
+    {
+        currentTime = goalTime;
+        slider.value = 0;
+    }
+    public void clickButton()
+    {
+        slider.value += 0.1f;
+    }
+
+    public void StartGame(GameObject FireGameUI)
+    {
+        this.FireGameUI = FireGameUI;
+        isGame = true;
+        this.FireGameUI.SetActive(true);
+    }
+    public void StopGame()
+    {
+        isGame = false;
+        FireGameUI.SetActive(false);
+        GameManager.isPause = false;
+        _player.GetComponent<ThirdPersonController>().enabled = true;
+    }
+}
